7a290725eb456d2449292a5ae107c24dd65adc14
Chris Mbah <mbahchris46@gmail.com>
2023-09-03 20:45:25 +0100

    Routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Home from './components/Home/Home'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
-    <>
-      Hello World 
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </Router>
   )
 }
 
