93159f813598d8463e6a300079bc8eaacac5c565
Chris Mbah <mbahchris46@gmail.com>
2023-09-06 10:38:43 +0100

    Minor changes

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -12,14 +12,8 @@ export default function BudgetProvider({children}:BudgetProviderProps) {
   const [userName,setUsername]=useState<string>("")
   const [firstname,setFirstname]=useState<string>("")
 
-  function updateUser(fullName:string):void{
-    setUsername(fullName)
-    const fullNameParts=fullName.split(" ")
-    setFirstname(fullNameParts[0])
-    localStorage.setItem("firstname",fullNameParts[0])
-  }
  
-  const contextValue:any={userName,setUsername,firstname,setFirstname,updateUser}
+  const contextValue:any={userName,setUsername,firstname,setFirstname}
 
   return (
     <BudgetContextApp.Provider value={contextValue} >
