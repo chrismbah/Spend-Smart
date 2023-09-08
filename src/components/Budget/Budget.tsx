2924336346ebb6059c944d95c2a9f2cf0e0848bb
Chris Mbah <mbahchris46@gmail.com>
2023-09-08 13:21:40 +0100

    Implemented budget component

--- src/components/Budget/Budget.tsx
+++ src/components/Budget/Budget.tsx
@@ -1,5 +1,7 @@
-import React,{useEffect} from 'react'
-import { useBudgetContext } from '../../context/Context'
+// import React,{useEffect} from 'react'
+// import { useBudgetContext } from '../../context/Context'
+import "./Budget.css"
+import Trash from "../../icons/Trash"
 
 
 export default function Budget() {
@@ -8,9 +10,26 @@ export default function Budget() {
     const budgetAmount=localStorage.getItem("budget-amount")
 
   return (
-    <div>
-      {budgetName}
-      {budgetAmount}
+    <div className="budget">
+      <div className="overview-title">
+       <span>{budgetName}</span> Overview
+      </div>
+      <div className="overview">
+        <div className="overview-info">
+          <div className="name">
+            {budgetName}
+          </div>
+          <div className="amount">
+            ${Number(budgetAmount).toLocaleString()}
+          </div>
+        </div>
+        <div className="progress">
+          <div className="progress-bar"></div>
+        </div>
+        <div className="clear-btn">
+          <button className="clear">Clear <Trash /></button>
+        </div>
+      </div>
     </div>
   )
 }
