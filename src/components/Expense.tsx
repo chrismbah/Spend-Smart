17a5817182e8fb6306b41abeb2d4b85dfc3cb62f
Chris Mbah <mbahchris46@gmail.com>
2023-09-11 15:18:04 +0100

    Delete old files

--- src/components/Expense.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { ExpenseListProps } from '../interface/Interfaces'
-
-export default function Expense({name,amount}:ExpenseListProps) {
-  return (
-    <div className='expense'>Expense of {name} and price of {amount}</div>
-  )
-}
